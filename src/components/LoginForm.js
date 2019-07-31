2146109b9c084591eba3dea636080a1f3af118bd
Vinh Tran <vincen7.tran@gmail.com>
2019-07-31 12:19:29 -0700

    input field set states

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, TextInput } from 'react-native';
 
 import { Button, Card, CardSection } from './common';
 
 class LoginForm extends React.Component {
+  state = {
+    text: ''
+  }
+
   render() {
+    const { } = styles;
+    const { text } = this.state;
+
     return (
       <Card>
         <CardSection>
-
+          <TextInput
+            value={text}
+            onChangeText={text => this.setState({ text })}
+            style={{ height: 20, width: 100 }}
+          />
         </CardSection>
 
         <CardSection>
@@ -23,6 +34,10 @@ class LoginForm extends React.Component {
       </Card>
     );
   }
+};
+
+const styles = {
+
 };
 
 export default LoginForm;
