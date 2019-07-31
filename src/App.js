53f680d3fd87f7678c2a4de5b452d510ac20c00a
Vinh Tran <vincen7.tran@gmail.com>
2019-07-31 14:13:01 -0700

    firebase listener added

--- src/App.js
+++ src/App.js
@@ -3,23 +3,60 @@ import { View } from 'react-native';
 import { registerRootComponent } from 'expo';
 import firebase from 'firebase';
 import firebaseConfig from '../firebaseConfig';
-import { Header } from './components/common';
+import { Header, Button, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends React.Component {
+  state = {
+    loggedIn: null
+  }
+
   componentDidMount() {
     firebase.initializeApp(firebaseConfig);
+
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) this.setState({ loggedIn: true });
+      else this.setState({ loggedIn: false });
+    });
+  }
+
+  renderContent = () => {
+    const { spinnerStye } = styles;
+    const { loggedIn } = this.state;
+
+    switch (loggedIn) {
+      case true:
+        return (
+          <Button>
+            Log Out
+          </Button>
+        );
+      case false:
+        return <LoginForm />
+      default:
+        return (
+          <View style={spinnerStye}>
+            <Spinner size="large" />
+          </View>
+        );
+    };
+
   }
 
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
+}
 
+const styles = {
+  spinnerStye: {
+    marginTop: '50%'
+  }
 }
 
 export default registerRootComponent(App);
